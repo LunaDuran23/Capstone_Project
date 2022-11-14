952c2017081c4a82829a3c49d8dffc608b119dad
jguevaraangel22 <juana.guevara@urosario.edu.co>
2022-11-14 16:13:02 -0500

    Log Out added

--- frontend/src/Components/ChartVotes.tsx
+++ frontend/src/Components/ChartVotes.tsx
@@ -1,38 +1,25 @@
 import React from 'react';
 
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  Cell,
-  CartesianGrid,
-  Tooltip,
-  Legend
-} from "recharts";
+import { BarChart, Bar, XAxis, YAxis, Cell, CartesianGrid, Tooltip, Legend } from 'recharts';
 
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
-const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
-
-const ChartVotes = ({data}) =>{
-    
-    return(
-        <BarChart 
-                width={900}
-                height={400}
-                data={data}
-                margin={{ top: 25, right: 0, left: 0, bottom: 25 }}
-            >
-                <Bar dataKey="votes" fontFamily='sans-serif' fill="#00a0fc" >
+const ChartVotes = ({ data }) => {
+    return (
+        <>
+            <BarChart width={900} height={400} data={data} margin={{ top: 25, right: 0, left: 0, bottom: 25 }}>
+                <Bar dataKey="votes" fontFamily="sans-serif" fill="#00a0fc">
                     {/*data.map((entry, index) => (
                         <Cell key={`cell-${index}`} fill={COLORS[index % 20]} />
                     ))*/}
-                </Bar> 
-                <XAxis dataKey="name" fontFamily="sans-serif"  />
-                
+                </Bar>
+                <XAxis dataKey="name" fontFamily="sans-serif" />
+
                 <YAxis />
             </BarChart>
+        </>
     );
-}
+};
 
-export default ChartVotes;
+// const ChartVotes = null;
+// export default ChartVotes;
--- frontend/src/Components/NavB.tsx
+++ frontend/src/Components/NavB.tsx
@@ -6,64 +6,56 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import "./App.css"
-import urosario from "./urosario.png";
-
+import './App.css';
+import urosario from './urosario.png';
 
 const NavB = () => {
-    const token_id = localStorage.getItem("token");
+    const token_id = localStorage.getItem('token');
 
-    console.log("Este es el token_id : ", token_id)
+    const handleClickEvent = (event) => {
+        localStorage.setItem('token', '');
+    };
+
+    console.log('Este es el token_id : ', token_id);
     let navigate = useNavigate();
-    if (token_id === "" || token_id === null) {
-        return(
-                <Navbar fixed="top" collapseOnSelect expand="lg" variant="dark" className="color-nav">
-                    <Container>
+    if (token_id === '' || token_id === null) {
+        return (
+            <Navbar fixed="top" collapseOnSelect expand="lg" variant="dark" className="color-nav">
+                <Container>
                     <Navbar.Brand href="/">
-                    <img
-                        src={urosario}
-                        width="200"
-                        height="90"
-                        className="d-inline-block align-top"
-                        alt=""
-                    />
+                        <img src={urosario} width="200" height="90" className="d-inline-block align-top" alt="" />
                     </Navbar.Brand>
-                    <Form className="form_pos">
-                        <Button onClick={() => navigate('/votaciones')} variant="primary" >Votaciones</Button>{' '}
-                    </Form>                                        
                     <Nav>
                         <Nav.Link href="/resultados">Consultar Resultados</Nav.Link>
                         <Nav.Link href="/logIn">Log In</Nav.Link>
                         <Nav.Link href="/signUp">Sign Up</Nav.Link>
                     </Nav>
-                    </Container>
-                </Navbar>
-        );
-    }else{
-        return(
-            <Navbar fixed="top" collapseOnSelect expand="lg" variant="dark" className="color-nav">
-                <Container>
-                <Navbar.Brand href="/">
-                <img
-                    src={urosario}
-                    width="200"
-                    height="90"
-                    className="d-inline-block align-top"
-                    alt=""
-                />
-                </Navbar.Brand>
-                <Form className="form_pos">
-                    <Button onClick={() => navigate('/votaciones')} variant="primary" >Votaciones</Button>{' '}
-                </Form>                                        
-                <Nav>
-                    <Nav.Link href="/resultados">Consultar Resultados</Nav.Link>
-                </Nav>
                 </Container>
             </Navbar>
-    );
-    
+        );
+    } else {
+        return (
+            <Navbar fixed="top" collapseOnSelect expand="lg" variant="dark" className="color-nav">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img src={urosario} width="200" height="90" className="d-inline-block align-top" alt="" />
+                    </Navbar.Brand>
+                    <Form className="form_pos">
+                        <Button onClick={() => navigate('/votaciones')} variant="primary">
+                            Votaciones
+                        </Button>{' '}
+                    </Form>
+                    <Nav>
+                        <Nav.Link href="/resultados">Consultar Resultados</Nav.Link>
+                        <Nav.Link href="/" onClick={handleClickEvent}>
+                            Log Out
+                        </Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        );
     }
-}
+};
 export default NavB;
