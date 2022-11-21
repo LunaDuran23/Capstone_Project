35c7d96875fb30992ff699482ed673434a8e41e5
Luis De Ávila <luis.deavila@urosario.edu.co>
2022-11-21 20:26:15 +0000

    Added endpoints for obtaining voting info

--- backend/routes/votinginfo.py
+++ backend/routes/votinginfo.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
-from backend.common.common import CANDIDATE_COLLECTION, FACULTY_COLLECTION, VOTING_LIST_COLLECTION
+from backend.common.common import (
+    CANDIDATE_COLLECTION, FACULTY_COLLECTION,
+    VOTING_LIST_COLLECTION, VOTING_ROUND_COLLECTION, VOTING_URNS_COLLECTION
+)
 from backend.common.types import RequestWithDB
 from typing import List
 from backend.data.model import VotingListOut, BaseResponse, Candidate, FacultyOut
@@ -62,3 +65,75 @@ def get_proposals(voting_list_id: int, request: RequestWithDB):
                             msg="Voting list with given ID doesn't exist!")
 
     return BaseResponse(success=True, msg="", payload=voting_list["proposals"])
+
+
+@router.get(
+    "/get_current_round_status",
+    summary="Get Round Status",
+    description="Get the current vote status for the given round id",
+)
+def get_voting_results(round_id: int, request: RequestWithDB):
+    voting_urn = request.app.database[VOTING_ROUND_COLLECTION].find_one(
+        {"round_id": round_id})
+
+    if voting_urn is None:
+        return BaseResponse(
+            success=False, payload=None,
+            msg="Voting round with given ID doesn't exist!")
+
+    def urn_to_list(urnid):
+        # get urn
+        found_urn = request.app.database[VOTING_URNS_COLLECTION].find_one({
+            "_id": urnid})
+        if found_urn is None:
+            return BaseResponse(success=False, payload=None, msg="Failed to get urn results!")
+        # map urn candidate names to their votes
+        urnlist = []
+        for i in range(len(found_urn["choices"])):
+            votecount = found_urn["currentVotes"][i]
+            choice = found_urn["choices"][i]
+            candidate = request.app.database[CANDIDATE_COLLECTION].find_one(
+                {'_id': choice})
+            if candidate is None:
+                return BaseResponse(success=False, payload=None, msg="Failed to get urn results!")
+            urnlist.append(
+                {'name': candidate['name'] + ' ' + candidate['surname'], 'votes': votecount})
+        return urnlist
+
+    resp_dict = {}
+    presurn = voting_urn['presidential_urn']
+    resp_dict["formula"] = urn_to_list(presurn)
+    resp_dict["Primer Semestre"] = urn_to_list(voting_urn['semestral_urns'][0])
+    resp_dict["Segundo Semestre"] = urn_to_list(
+        voting_urn['semestral_urns'][1])
+    resp_dict["Tercer Semestre"] = urn_to_list(voting_urn['semestral_urns'][2])
+    resp_dict["Cuarto Semestre"] = urn_to_list(voting_urn['semestral_urns'][3])
+    resp_dict["Quinto Semestre"] = urn_to_list(voting_urn['semestral_urns'][4])
+    resp_dict["Sexto Semestre"] = urn_to_list(voting_urn['semestral_urns'][5])
+    resp_dict["Septimo Semestre"] = urn_to_list(
+        voting_urn['semestral_urns'][6])
+    resp_dict["Octavo Semestre"] = urn_to_list(voting_urn['semestral_urns'][7])
+
+    return BaseResponse(success=True, msg="", payload=resp_dict)
+
+
+@router.get(
+    "/get_all_round_status",
+    summary="Get All Rounds Status",
+    description="Get the current vote status ALL rounds. Keys will be displayed by the faculty name of the round",
+)
+def get_all_voting_results(request: RequestWithDB):
+    all_rounds = list(request.app.database[VOTING_ROUND_COLLECTION].find({}))
+    respdict = {}
+    for round in all_rounds:
+        # get faculty name
+        faculty_id = round["faculty_id"]
+        faculty = request.app.database[FACULTY_COLLECTION].find_one(
+            {'faculty_id': faculty_id})
+        if faculty is None:
+            return BaseResponse(success=False, msg="Internal Server error fail! Broken voting round.", payload=None)
+        roundinfo = get_voting_results(
+            round["round_id"], request).payload
+
+        respdict[faculty['name']] = roundinfo
+    return BaseResponse(success=True, msg="", payload=respdict)
--- face_memes/realTimeFaceRecognition_old.py
+++ face_memes/realTimeFaceRecognition_old.py
@@ -35,6 +35,7 @@ for name in mylistdir(knownFacesFolder): # iterate over known identities inside 
         print(f'Encoding: {name}/{fileName}')
         image = face_recognition.load_image_file(f"./{knownFacesFolder}/{name}/{fileName}") #load image
         encoding = face_recognition.face_encodings(image)[0] #encode, In case tgheres more than one face it will only tak,e the first opne detected
+        print(f'el tipo de dato del enconding es: {type(encoding)}')
         print(f'\t Faces detected: {len(encoding)/128}')
         #add info to lists
         knownFaces.append(encoding)
@@ -42,15 +43,20 @@ for name in mylistdir(knownFacesFolder): # iterate over known identities inside 
 
 print("... Known faces loaded!\n\n")
 
-print("Loading unknown faces...")
-for fileName in mylistdir(unknownFacesFolder):
-    print(f'Encoding: {unknownFacesFolder}/{fileName}')
+cap = cv2.VideoCapture(0) #if want to use video replace 0 with 'filename.mp3'
+t= True
 
-    image = face_recognition.load_image_file(f"{unknownFacesFolder}/{fileName}")
+while t:
+    _, image = cap.read()
 
     unknownFaceLocations = face_recognition.face_locations(image)
     unknownFaceEncodings = face_recognition.face_encodings(image, unknownFaceLocations)
-    print(f'\t Faces detected: {len(unknownFaceEncodings)/128}')
+
+    cv2.imshow('image', image)
+    
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
 
     for faceEncoding in unknownFaceEncodings:
         matches = face_recognition.compare_faces(knownFaces, faceEncoding, tolerance)
@@ -62,13 +68,3 @@ for fileName in mylistdir(unknownFacesFolder):
                 name = knownNames[bestMatchIndex]
 
     print(f"{name} es quien aparece en la foto")
-
-
-
-
-
-
-
-
-
-#para mas adelante, la cara que debo reconocer debe estar en la carpeta de caras desconocidas
